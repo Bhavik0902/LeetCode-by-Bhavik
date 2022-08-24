d39304b4d336456187fe087c43fdde3cfed414d1
Bhavik0902 <76263196+Bhavik0902@users.noreply.github.com>
2022-08-24 21:28:37 +0530

    Time: 22 ms (25.66%), Space: 7.8 MB (7.88%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.cpp
+++ 242-valid-anagram/242-valid-anagram.cpp
@@ -2,7 +2,6 @@ class Solution {
 public:
     bool isAnagram(string s, string t) {
         vector<char> vec1, vec2;
-        //bool res = true;
         
         if(t.length()>s.length())
             return false;
@@ -12,13 +11,7 @@ public:
         
         sort(vec1.begin(), vec1.end());
         for(char c: t)
-        {
             vec2.emplace_back(c);
-            // if(find(vec1.begin(),vec1.end(),c)==vec1.end() || find(vec2.begin(),vec2.end(),c)!=vec2.end()){
-            //     res=false;
-            //     break;
-            // }
-        }
         
         sort(vec2.begin(), vec2.end());
         if(vec1==vec2)
